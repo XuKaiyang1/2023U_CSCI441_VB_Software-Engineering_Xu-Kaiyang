1f594e6aa5c8e94aa4ae9732d1c18df4fa5d4bdd
XuKaiyang1 <142872263+XuKaiyang1@users.noreply.github.com>
2023-08-27 03:47:41 +0800

    Update unit_test.py

--- 2_unit_testing/unit_test.py
+++ 2_unit_testing/unit_test.py
@@ -1,4 +1,4 @@
-# unit_test.py
+
 import unittest
 from main_script import AgriSense
 class TestAgriSense(unittest.TestCase):
@@ -7,10 +7,14 @@ class TestAgriSense(unittest.TestCase):
     def test_get_real_time_soil_moisture(self):
         moisture = self.agrisense.get_real_time_soil_moisture()
         self.assertTrue(0 <= moisture <= 100, "Soil moisture level should be between 0% and 100%.")
-    def test_provide_irrigation_advice(self):
-        advice_dry = self.agrisense.provide_irrigation_advice()
-        self.agrisense.soil_moisture_level = 50
-        advice_adequate = self.agrisense.provide_irrigation_advice()
-        self.assertNotEqual(advice_dry, advice_adequate, "Advice should differ based on moisture level.")
+def test_provide_irrigation_advice(self):
+    self.agrisense.soil_moisture_level = 30  # Simulate a moisture level below 40%
+    advice_dry = self.agrisense.provide_irrigation_advice()
+
+    self.agrisense.soil_moisture_level = 50  # Simulate a moisture level above 40%
+    advice_adequate = self.agrisense.provide_irrigation_advice()
+
+    self.assertNotEqual(advice_dry, advice_adequate, "Advice should differ based on moisture level.")
+
 if __name__ == "__main__":
     unittest.main()
